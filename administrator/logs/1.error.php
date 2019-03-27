08c1ed3197a2dd557c1320b8aa54e713bff3a77f
jeffxsi <jfbranchaud40@gmail.com>
2019-03-27 17:36:54 -0400

    premier commit

---
+++ administrator/logs/1.error.php
@@ -0,0 +1,12 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2019-02-15 15:31:21 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2019-02-15T15:31:21+00:00	INFO 206.167.42.70	joomlafailure	Empty password not allowed.
+2019-02-22T13:25:47+00:00	INFO 206.167.42.53	joomlafailure	Username and password do not match or you do not have an account yet.
+2019-02-22T13:26:07+00:00	INFO 206.167.42.53	joomlafailure	Username and password do not match or you do not have an account yet.
+2019-02-22T13:26:12+00:00	INFO 206.167.42.53	joomlafailure	Empty password not allowed.
+2019-02-22T13:26:24+00:00	INFO 206.167.42.53	joomlafailure	Username and password do not match or you do not have an account yet.
+2019-02-22T13:27:20+00:00	INFO 206.167.42.53	joomlafailure	Username and password do not match or you do not have an account yet.
---
+++ administrator/logs/1.joomla_update.php
@@ -0,0 +1,17 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2019-02-15 15:31:54 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2019-02-15T15:31:54+00:00	INFO 206.167.42.70	update	Update started by user Super User (419). Old version is 3.9.2.
+2019-02-15T15:31:55+00:00	INFO 206.167.42.70	update	Downloading update file from https://s3-us-west-2.amazonaws.com/joomla-official-downloads/joomladownloads/joomla3/Joomla_3.9.3-Stable-Update_Package.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIZ6S3Q3YQHG57ZRA%2F20190215%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20190215T153154Z&X-Amz-Expires=60&X-Amz-SignedHeaders=host&X-Amz-Signature=b75c13bc4d789d7a1307ab0b73bb30dff907e044361e80883281db60b177e764.
+2019-02-15T15:31:56+00:00	INFO 206.167.42.70	update	File Joomla_3.9.3-Stable-Update_Package.zip downloaded.
+2019-02-15T15:31:56+00:00	INFO 206.167.42.70	update	Starting installation of new version.
+2019-02-15T15:32:06+00:00	INFO 206.167.42.70	update	Finalising installation.
+2019-02-15T15:32:06+00:00	INFO 206.167.42.70	update	Ran query from file 3.9.3-2019-01-12. Query text: UPDATE `#__extensions`  SET `params` = REPLACE(`params`, '"com_categories",', '".
+2019-02-15T15:32:06+00:00	INFO 206.167.42.70	update	Ran query from file 3.9.3-2019-01-12. Query text: INSERT INTO `#__action_logs_extensions` (`extension`) VALUES ('com_checkin');.
+2019-02-15T15:32:06+00:00	INFO 206.167.42.70	update	Ran query from file 3.9.3-2019-02-07. Query text: INSERT INTO `#__postinstall_messages` (`extension_id`, `title_key`, `description.
+2019-02-15T15:32:06+00:00	INFO 206.167.42.70	update	Deleting removed files and folders.
+2019-02-15T15:32:07+00:00	INFO 206.167.42.70	update	Cleaning up after installation.
+2019-02-15T15:32:07+00:00	INFO 206.167.42.70	update	Update to version 3.9.3 is complete.
